aae42b1f33854c2a632048422e850f2917e60560
Jan Schneider <jan.schneider@codecentric.de>
2025-04-20 17:15:35 +0200

    nun wir fuer jeden Schritt die selben samples verwendet

--- excercise1/excercise1.jl
+++ excercise1/excercise1.jl
@@ -1,32 +1,59 @@
 using StatsPlots, Distributions
 
 numberOfSimulations = 50
+alpha = 1
+beta = 1
+
+dir = @__DIR__
+
+prior = Beta(alpha, beta)
+bernoulli = Bernoulli(0.7)
+
+data = rand(Binomial(1, 0.7), numberOfSimulations)
+
+sample1 = data[1]
+posterior1 = Beta(alpha + sample1, beta + 1 - sample1)
+
+sample5 = sum(data[1:5])
+posterior5 = Beta(alpha + sample5, beta + 5 - sample5)
+
+sample50 = sum(data)
+posterior50 = Beta(alpha + sample50, beta + 50 - sample50)
+
+
+plot(prior, label="prior", legend=:topleft)
+plot!(posterior50, label="50 ($(data[1:5]))")
+plot!(posterior5, label="5 ($(data[1:5]))")
+plot!(posterior1, label="1 ($(data[1]))")
+
+vspan!([0.69, 0.71]; alpha = 0.5, label = "true θ", color="green")
+
+savefig(joinpath(dir, "4c.png"))
+
 alpha = 2
 beta = 2
 
 prior = Beta(alpha, beta)
-bernoulli = Bernoulli(0.7)
-OurBinominal(n) = Binomial(n, 0.7)
 
-sample50 = rand(OurBinominal(50))
-posterior50 = Beta(alpha + sample50, beta + 50 - sample50)
-
-sample5 = rand(OurBinominal(5))
-posterior5 = Beta(alpha + sample5, beta + 5 - sample5)
-
-sample1 = rand(OurBinominal(1))
+sample1 = data[1]
 posterior1 = Beta(alpha + sample1, beta + 1 - sample1)
 
-println(posterior)
+sample5 = sum(data[1:5])
+posterior5 = Beta(alpha + sample5, beta + 5 - sample5)
 
-plot(prior, label="prior")
-plot!(posterior50, label="50")
-plot!(posterior5, label="5")
-plot!(posterior1, label="1")
+sample50 = sum(data)
+posterior50 = Beta(alpha + sample50, beta + 50 - sample50)
+
+plot(prior, label="prior", legend=:topleft)
+plot!(posterior50, label="50 ($(data[1:5]))")
+plot!(posterior5, label="5 ($(data[1:5]))")
+plot!(posterior1, label="1 ($(data[1]))")
 
 vspan!([0.69, 0.71]; alpha = 0.5, label = "true θ", color="green")
 
-# savefig("Aufgage 4d")
+savefig(joinpath(dir, "4d.png"))
+
+
 
 
 
--- excercise1/exercise1.md
+++ excercise1/exercise1.md
@@ -6,9 +6,16 @@
 
 ![](4c.png)
 
+Je mehr Samples man zieht, desto mehr nähert sich die Verteilung des Posteriors dem tatsächlichen `p` an.
+
 
 ### d)
 
 ![](4d.png)
+
+Der neue Prior ist nun keine Gleichverteilung mehr, sondern ähenlt der tatsächlichen Verteilung nun schon etwas mehr. Der Unterschied lässt sich gut an dem Posterior nach einem Experiment sehen (lila Kurve). Wo oben nur eine schiefe Linie zu sehen ist, ist der Posterior bei einem informierten Prior schon nach einem Experiment dem tatsächlichen Wert viel näher. Nach 5 oder auch 50 Experimenten ist der Unterschied aber nicht mehr auszumachen.
+
+### e)
+
 
 
