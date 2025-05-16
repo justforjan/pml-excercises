4712363e77e759dbc6ba561561e4a80b1ac6871d
justforjan <schneiderjan.2013@gmail.com>
2025-05-16 15:10:22 +0200

    calculated marginals with data but sum product still wrong

--- exercise2/exercise2-3.jl
+++ exercise2/exercise2-3.jl
@@ -7,11 +7,18 @@ using .DistributionCollections: DistributionBag, add!, reset!
 using Distributions
 
 N = 10
-mu1 = 9
-mu2 = 4
-v1 = 1
-v2 = 1
+mu1 = 1
+mu2 = 9
+v1 = 2
+v2 = 2
 bq = 1
+
+println("N = $(N)")
+println("μ1 = $(mu1)")
+println("μ2 = $(mu2)")
+println("v1 = $(v1)")
+println("v2 = $(v2)")
+println("β^2 = $(bq)")
 
 values = 1:N
 dvalues = -N+1:N-1
@@ -113,6 +120,93 @@ println("p1: $(DiscreteFromNonLog(marginal_p1))")
 println("p2: $(DiscreteFromNonLog(marginal_p2))")
 println("d: $(DiscreteFromNonLog(marginal_d))")
 
+function p_s1(s1)
+    local sum = 0
+    for s2 in values
+        for p1 in values
+            for p2 in values
+                for d in dvalues
+                    sum += f1(s1) * f2(s1, p1) * f3(s2) * f4(s2, p2) * f5(p1, p2, d) * f6(d)
+                end
+            end
+        end
+    end
+    sum
+end
+
+function p_s2(s2)
+    local sum = 0
+    for s1 in values
+        for p1 in values
+            for p2 in values
+                for d in dvalues
+                    sum += f1(s1) * f2(s1, p1) * f3(s2) * f4(s2, p2) * f5(p1, p2, d)  * f6(d)
+                end
+            end
+        end
+    end
+    sum
+end
+
+function p_p1(p1)
+    local sum = 0
+    for s1 in values
+        for s2 in values
+            for p2 in values
+                for d in dvalues
+                    sum += f1(s1) * f2(s1, p1) * f3(s2) * f4(s2, p2) * f5(p1, p2, d) * f6(d)
+                end
+            end
+        end
+    end
+    sum
+end
+
+function p_p2(p2)
+    local sum = 0
+    for s1 in values
+        for s2 in values
+            for p1 in values
+                for d in dvalues
+                    sum += f1(s1) * f2(s1, p1) * f3(s2) * f4(s2, p2) * f5(p1, p2, d) * f6(d)
+                end
+            end
+        end
+    end
+    sum
+end
+
+function p_d(d)
+    local sum = 0
+    for s1 in values
+        for s2 in values
+            for p1 in values
+                for p2 in values
+                    sum += f1(s1) * f2(s1, p1) * f3(s2) * f4(s2, p2) * f5(p1, p2, d) * f6(d)
+                end
+            end
+        end
+    end
+    sum
+end
+
+println()
+println("All Marginals *with* data by summing out:")
+marginal_s1 = p_s1.(values)
+marginal_s2 = p_s2.(values)
+marginal_p1 = p_p1.(values)
+marginal_p2 = p_p2.(values)
+marginal_d = p_d.(dvalues)
+
+println("s1: $(DiscreteFromNonLog(marginal_s1))")
+println("s2: $(DiscreteFromNonLog(marginal_s2))")
+println("p1: $(DiscreteFromNonLog(marginal_p1))")
+println("p2: $(DiscreteFromNonLog(marginal_p2))")
+println("d: $(DiscreteFromNonLog(marginal_d))")
+
+println()
+println("----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
 # Aufgabe 3 b)
 
 marginals = DistributionBag(Discrete(N))
@@ -140,10 +234,10 @@ m_P2_f4 = Discrete(N)
 m_P2_f5 = Discrete(N)
 m_f5_P2 = Discrete(N)
 
-m_f5_D = Discrete(2*N-1)
-m_D_f5 = Discrete(2*N-1)
-m_D_f6 = Discrete(2*N-1)
-m_f6_D = Discrete(2*N-1)
+m_f5_D = Discrete(length(dvalues))
+m_D_f5 = Discrete(length(dvalues))
+m_D_f6 = Discrete(length(dvalues))
+m_f6_D = Discrete(length(dvalues))
 
 
 # 1
@@ -216,16 +310,74 @@ println("P1: $(marginals[3])")
 println("P2: $(marginals[4])")
 println("D: $(marginal_d)")
 
-println()
-
 
 # 13
-@show m_f6_D = DiscreteFromNonLog(f6.(dvalues))
+m_f6_D = DiscreteFromNonLog(f6.(dvalues))
 
 # 14
-@show marginal_d = m_f5_D * m_f6_D
+marginal_d = m_f5_D * m_f6_D
 
 # 15
-println(marginal_d)
-println(m_f5_D)
 m_D_f5 = marginal_d / m_f5_D
+
+# 16
+res = zeros(N)
+
+for p1 in values
+    for (di, d) in enumerate(dvalues)
+        for p2 in values
+            res[p1] += f5(p1, p2, d) * ℙ(m_D_f5)[di] * ℙ(m_P2_f5)[p2]
+        end
+    end
+end
+
+m_f5_P1 = DiscreteFromNonLog(res)   
+
+# 17
+res = zeros(N)
+for p2 in values
+    for (di, d) in enumerate(dvalues)
+        for p1 in values
+            res[p2] += f5(p1, p2, d) * ℙ(m_D_f5)[di] * ℙ(m_P1_f5)[p1]
+        end
+    end
+end
+m_f5_P2 = DiscreteFromNonLog(res)
+
+# 18
+marginals[3] = m_f2_P1 * m_f5_P1
+
+# 19
+marginals[4] = m_f4_P2 * m_f5_P2
+
+# 20
+res = zeros(N)
+for s1 in values
+    for p1 in values
+        res[s1] += f2(s1, p1) * ℙ(marginals[3] / m_f2_P1)[p1]
+    end
+end
+m_f2_S1 = DiscreteFromNonLog(res)
+
+# 21
+res = zeros(N)
+for s2 in values
+    for p2 in values
+        res[s2] += f2(s2, p2) * ℙ(marginals[4] / m_f4_P2)[p2]
+    end
+end
+m_f2_S1 = DiscreteFromNonLog(res)
+
+# 22
+marginals[1] = m_f1_S1 * m_f2_S1
+
+# 23
+marginals[2] = m_f3_S2 * m_f4_S2
+
+println()
+println("All Marginals *with* data with Sum-Product:")
+println("S1: $(marginals[1])")
+println("S2: $(marginals[2])")
+println("P1: $(marginals[3])")
+println("P2: $(marginals[4])")
+println("D: $(marginal_d)")
